fe695f447922893674dfe39e60780502f2a3a441
katayama <idnlnroocwetn22@gmail.com>
2025-09-12 00:23:16 +0900

    [add] implement scanning parentheses

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::env;
+use std::fmt;
 use std::fs;
 use std::io::{self, Write};
+
+enum Token {
+    LeftParen,
+    RightParen,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Token::LeftParen => write!(f, "LEFT_PAREN ( null"),
+            Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,14 +35,25 @@ fn main() {
             });
 
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
+                interpret_tokens(file_contents);
             }
+            println!("EOF  null");
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
         }
+    }
+}
+
+/// Interpreter that processes tokens and prints them.
+fn interpret_tokens(tokens: String) {
+    for token in tokens.chars() {
+        let token = match token {
+            '(' => Token::LeftParen,
+            ')' => Token::RightParen,
+            _ => unimplemented!(),
+        };
+        println!("{}", token);
     }
 }
