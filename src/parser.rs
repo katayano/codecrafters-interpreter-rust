75dcfbb849bc9166866be2dd77568d5c2234b13a
katayama <idnlnroocwetn22@gmail.com>
2025-10-13 21:35:07 +0900

    [add] support for parsing number literals

--- src/parser.rs
+++ src/parser.rs
@@ -11,6 +11,15 @@ impl fmt::Display for Parser {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self.token {
             Token::ReservedWord(word) => write!(f, "{}", word),
+            Token::NumberLiterals(num_literal) => {
+                // For Example, "123" -> "123.0", "123.45" -> "123.45", "123.000" -> "123.0"
+                let num = num_literal.parse::<f64>().unwrap();
+                if num.to_string().contains(".") {
+                    write!(f, "{}", num)
+                } else {
+                    write!(f, "{}.0", num)
+                }
+            }
             _ => unimplemented!(),
         }
     }
